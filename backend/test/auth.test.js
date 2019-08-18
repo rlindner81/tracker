239b7ca2eaf509c97fd431266789dd36cbc74db6
Richard Lindner <rlindner81@gmail.com>
2019-08-18 17:14:09 +0200

    Baby steps

--- backend/test/auth.test.js
+++ backend/test/auth.test.js
@@ -2,55 +2,65 @@ const http = require("http")
 const request = require("supertest")
 const app = require("../src/app")
 
-describe("Auth APIs", function() {
+const user = {
+  name: "test",
+  email: "test@test.com",
+  password: "test"
+}
+
+describe("Auth APIs", function () {
   let server
+
   beforeAll(done => {
     server = http.createServer(app)
     server.listen(done)
   })
+
   afterAll(done => {
     server.close(done)
   })
 
-  it("POST /api/auth/register", async function(done) {
-    const user = {
-      name: "test",
-      email: "test@test.com",
-      password: "test"
-    }
-    const res = await request(app)
+  it("POST /api/auth/register", async function () {
+    const res = await request(server)
       .post("/api/auth/register")
-      .set("Accept", "application/json")
       .send(user)
+      .set("Accept", "application/json")
       .expect("Content-Type", /json/)
       .expect(200)
     expect(res.body.name).toBe(user.name)
     expect(res.body.email).toBe(user.email)
     expect("password" in res.body).toBe(false)
-    done()
   })
 
-  // it('POST /api/auth/login', function (done) {
-  //   request(app)
-  //     .get('/users')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(200, done);
-  // });
+  it('POST /api/auth/login', async function () {
+    const res = await request(server)
+      .post("/api/auth/login")
+      .send({
+        nameOrEmail: user.name,
+        password: user.password
+      })
+      .expect("Content-Type", /text\/html/)
+      .expect(200)
+    expect(res.text).toBe("Login successful")
+  });
 
-  // it('POST /api/auth/logout', function (done) {
-  //   request(app)
-  //     .get('/users')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(200, done);
-  // });
+  it("GET /api/auth/me", async function () {
+    const res = await request(server)
+      .get("/api/auth/me")
+      // .expect("Content-Type", /json/)
+      // .expect(200)
+    expect(res.body.name).toBe(user.name)
+    expect(res.body.email).toBe(user.email)
+    expect("password" in res.body).toBe(false)
+  })
 
-  // it('GET /api/auth/me', function (done) {
-  //   request(app)
-  //     .get('/users')
-  //     .set('Accept', 'application/json')
-  //     .expect('Content-Type', /json/)
-  //     .expect(200, done);
-  // });
+  it('POST /api/auth/logout', async function (done) {
+    const res = await request(server)
+      .post("/api/auth/logout")
+      .send()
+      .expect("Content-Type", /text\/html/)
+      .expect(200)
+    expect(res.text).toBe("Logout successful")
+  });
+
 })
