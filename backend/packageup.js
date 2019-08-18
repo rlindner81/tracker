cae4e7c872ff534e20b85a05788b25bdea24d3d0
Richard Lindner <rlindner81@gmail.com>
2019-08-18 09:05:39 +0200

    Script to update dependencies properly

---
+++ backend/packageup.js
@@ -0,0 +1,12 @@
+const { unlinkSync } = require('fs');
+const { execSync } = require('child_process')
+const package = require("./package.json")
+
+for (const dep of Object.keys(package.dependencies)) {
+  execSync(`npm install --save ${dep}`, { stdio: "inherit" })
+}
+for (const dep of Object.keys(package.devDependencies)) {
+  execSync(`npm install --save-dev ${dep}`, { stdio: "inherit" })
+}
+unlinkSync("package-lock.json")
+execSync(`npm install --package-lock-only`, { stdio: "inherit" })
