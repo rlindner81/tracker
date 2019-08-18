460704aa36321d67f717347c936d3d9e60575b20
Richard Lindner <rlindner81@gmail.com>
2019-08-18 09:59:15 +0200

    webpack-dev-server needs funny setting https://github.com/webpack/webpack-dev-server/issues/2190#issuecomment-520670599

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   devServer: {
-    proxy: 'http://127.0.0.1:8000'
+    proxy: 'http://127.0.0.1:8000',
+    contentBase: './public'
   },
   chainWebpack: config => {
     config.module.rule('eslint').use('eslint-loader').options({
