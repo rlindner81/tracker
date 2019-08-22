2d6a03cdbc1cf80e164136bddfba063d2a7f4508
Richard Lindner <richard.lindner@sap.com>
2019-08-22 10:13:07 +0200

    Fixes #11

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,12 @@ module.exports = {
     proxy: 'http://127.0.0.1:8000',
     contentBase: './public'
   },
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true
+    }
+  },
   chainWebpack: config => {
     config.module.rule('eslint').use('eslint-loader').options({
       fix: true
