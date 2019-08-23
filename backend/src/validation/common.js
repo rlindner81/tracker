ccec71e3d69a4c13c2405b8c5839c5089592cd53
Richard Lindner <rlindner81@gmail.com>
2019-08-23 21:16:24 +0200

    Allow empty names. Fixes #19

--- backend/src/validation/common.js
+++ backend/src/validation/common.js
@@ -1,10 +1,7 @@
 const joi = require("@hapi/joi")
-const string = joi
-  .string()
-  .min(1)
-  .max(256)
-const alphanum = string.alphanum()
-const token = string.token()
+const string = joi.string().max(256)
+const stringNonEmpty = string.min(1)
+const token = stringNonEmpty.token()
 const fieldType = joi.string().valid("INPUT", "TEXT", "INTEGER", "FLOAT", "TIME")
 const generatorType = joi.string().valid("STATIC", "TIME_NOW", "TIME_RELATIVE_PREVIOUS")
 const inputType = token.uppercase() // joi.string().valid("FIELD", "SELECT")
@@ -13,7 +10,7 @@ const frequencyType = joi.string().valid("YEAR", "MONTH", "WEEK", "DAY", "HOUR",
 
 module.exports = {
   string,
-  alphanum,
+  stringNonEmpty,
   token,
   fieldType,
   generatorType,
