7c3ae2e6a22c0d3d7d0a84451674083b1d762a08
Richard Lindner <rlindner81@gmail.com>
2019-08-23 20:12:29 +0200

    Step value should be displayed for 'false' and '0' values

--- frontend/src/components/TrackList.vue
+++ frontend/src/components/TrackList.vue
@@ -13,8 +13,8 @@
         >
           <label>{{ track.fields.find(field => field.key === key).name }}</label>
           <span v-if="value === undefined || value === null">n/a</span>
-          <span v-if="value && getInputIdentifier(key) !== 'SELECT'">{{ value }}</span>
-          <span v-if="value && getInputIdentifier(key) === 'SELECT'">{{ getField(key).input.parameters.values.find(v => v.value.toString() === value.toString()).name }}</span>
+          <span v-if="value !== undefined && value !== null && getInputIdentifier(key) !== 'SELECT'">{{ value }}</span>
+          <span v-if="value !== undefined && value !== null && getInputIdentifier(key) === 'SELECT'">{{ getField(key).input.parameters.values.find(v => String(v.value) === String(value)).name }}</span>
         </div>
       </div>
       <div class="master-data">
