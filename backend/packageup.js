6920671d52034def0b8f74fc741970eb5cf299e8
Richard Lindner <rlindner81@gmail.com>
2019-08-26 14:58:02 +0200

    Update packageup.js

--- backend/packageup.js
+++ backend/packageup.js
@@ -1,8 +1,7 @@
-const { unlinkSync } = require("fs")
 const { execSync } = require("child_process")
 const package = require("./package.json")
-const dependencies = Object.keys(package.dependencies)
-const devDependencies = Object.keys(package.devDependencies)
+const dependencies = Object.keys(package.dependencies).filter(dep => dep.startsWith("^"))
+const devDependencies = Object.keys(package.devDependencies).filter(dep => dep.startsWith("^"))
 
 const run = cmd => {
   console.log(cmd)
@@ -11,5 +10,4 @@ const run = cmd => {
 
 run(`npm install --no-audit --no-package-lock --only=prod ${dependencies.map(dep => dep + "@latest").join(" ")}`)
 run(`npm install --no-audit --no-package-lock --only=dev ${devDependencies.map(dep => dep + "@latest").join(" ")}`)
-unlinkSync("package-lock.json")
 run("npm install --package-lock-only")
