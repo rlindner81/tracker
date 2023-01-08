490e67ea4df954d46c455a02d28de50d028866ae
Richard Lindner <rlindner81@gmail.com>
2023-01-08 17:42:27 +0100

    can do transfer for tracks

--- firebase-migration/src/migrate.mjs
+++ firebase-migration/src/migrate.mjs
@@ -1,11 +1,16 @@
 // https://firebase.google.com/docs/reference/admin
 // https://firebase.google.com/docs/reference/admin/node/firebase-admin.firestore
+// https://firebase.google.com/docs/reference/admin/node/firebase-admin.auth
+import { format } from "util";
 import { readFile } from "fs/promises";
 import admin from "firebase-admin";
 import { getApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import dataService from "./nedb-interface.cjs";
 
 const MAX_WRITES_PER_BATCH = 500; /** https://cloud.google.com/firestore/quotas#writes_and_transactions */
+const MIGRATE_USER = "sommernavi@gmail.com";
 
 const accountFilepath = new URL(
   "../../temp/trackit-f1b79-firebase-adminsdk-5o81p-fff95050db.json",
@@ -22,9 +27,9 @@ const chunk = (input, chunkSize) => {
   return result;
 };
 
-const deleteCollection = async (db, collection) => {
-  console.log("deleting collection '%s'", collection);
-  const snapshot = await db.collection(collection).get();
+const deleteCollection = async (db, colRef) => {
+  console.log("deleting collection '%s'", colRef.id);
+  const snapshot = await colRef.get();
   let records = 0;
 
   const batches = chunk(snapshot.docs, MAX_WRITES_PER_BATCH);
@@ -43,25 +48,100 @@ const deleteCollection = async (db, collection) => {
   console.log("deleted %i records", records);
 };
 
-const writeCollection = async (db, collection, data) => {
-  console.log("writing %i records to collection '%s'", data.length, collection);
-  const colRef = await db.collection(collection);
-  let records = 0;
+const createUserIdMap = async (app) => {
+  const auth = getAuth(app);
+  const userIdMap = {};
 
-  const batches = chunk(data, MAX_WRITES_PER_BATCH);
-  const commitBatchPromises = [];
+  const oldUsers = await dataService.users.find({}).execAsync();
+  const { users: newUsers } = await auth.listUsers();
+  for (const oldUser of oldUsers) {
+    const newUser = newUsers.find((newUser) => newUser.email === oldUser.email);
+    userIdMap[oldUser._id] = {
+      oldUser,
+      ...(newUser && { uid: newUser.uid, newUser }),
+    };
+  }
+  return userIdMap;
+};
 
-  batches.forEach((batch) => {
-    records += batch.length;
-    const writeBatch = db.batch();
-    batch.forEach((record) => {
-      writeBatch.create(colRef.doc(), record);
-    });
-    commitBatchPromises.push(writeBatch.commit());
-  });
+const _mapIdField = (field, object, map) => {
+  if (!map[object[field]].uid) {
+    throw new Error(
+      format(
+        "could not find matching uid for field %s %O",
+        field,
+        map[object[field]]
+      )
+    );
+  }
+  object[field] = map[object[field]].uid;
+};
 
-  await Promise.all(commitBatchPromises);
-  console.log("written %i records", records);
+const transferTracks = async (app, userIdMap) => {
+  const db = getFirestore(app);
+  const trackIdMap = {};
+
+  const oldTracks = await dataService.tracks.find({}).execAsync();
+  const tracksColRef = db.collection("tracks");
+
+  const count = (await tracksColRef.count().get()).data().count;
+  if (count !== 0) {
+    await deleteCollection(db, tracksColRef);
+  }
+
+  const batch = db.batch();
+  let batchCount = 0;
+
+  for (const oldTrack of oldTracks) {
+    const newTrack = { ...oldTrack };
+    Reflect.deleteProperty(newTrack, "_id");
+    _mapIdField("userId", newTrack, userIdMap);
+
+    const newTrackRef = tracksColRef.doc();
+    batch.create(newTrackRef, newTrack);
+    batchCount++;
+    if (batchCount >= MAX_WRITES_PER_BATCH) {
+      throw Error("too many operations in batch");
+    }
+
+    trackIdMap[oldTrack._id] = { uid: newTrackRef.uid, oldTrack, newTrack };
+  }
+  await batch.commit();
+  return trackIdMap;
+};
+
+const transferSteps = async (app, userIdMap, trackIdMap) => {
+  const db = getFirestore(app);
+  const stepIdMap = {};
+
+  const oldSteps = await dataService.steps.find({}).execAsync();
+  const stepsColRef = db.collection("steps");
+
+  const count = (await stepsColRef.count().get()).data().count;
+  if (count !== 0) {
+    await deleteCollection(db, stepsColRef);
+  }
+
+  const batch = db.batch();
+  let batchCount = 0;
+
+  for (const oldStep of oldSteps) {
+    const newStep = { ...oldStep };
+    Reflect.deleteProperty(newStep, "_id");
+    _mapIdField("userId", newStep, userIdMap);
+    _mapIdField("trackId", newStep, trackIdMap);
+
+    const newStepRef = stepsColRef.doc();
+    batch.create(newStepRef, newStep);
+    batchCount++;
+    if (batchCount >= MAX_WRITES_PER_BATCH) {
+      throw Error("too many operations in batch");
+    }
+
+    stepIdMap[oldStep._id] = { uid: newStepRef.uid, oldStep, newStep };
+  }
+  await batch.commit();
+  return stepIdMap;
 };
 
 const main = async () => {
@@ -72,10 +152,14 @@ const main = async () => {
     credential: admin.credential.cert(serviceAccount),
   });
   const app = getApp();
-  const db = getFirestore(app);
 
-  // await deleteCollection(db, "tracks");
-  await writeCollection(db, "tracks", [{ name: "a" }, { name: "b" }]);
+  console.log("creating user id map");
+  const userIdMap = await createUserIdMap(app);
+  console.log("transferring tracks");
+  const trackIdMap = await transferTracks(app, userIdMap);
+  console.log("transferred %i tracks", Object.keys(trackIdMap).length);
+  const stepIdMap = await transferSteps(app, userIdMap, trackIdMap);
+  console.log("transferred %i steps", Object.keys(stepIdMap).length);
 };
 
 main();
