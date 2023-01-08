1050ceda0c7d6cd8c32c67c3971c5c222bb8fc57
Richard Lindner <rlindner81@gmail.com>
2023-01-08 18:11:45 +0100

    Admin migrate script (#208)
    * beginning of migrate with admin
    
    * link to docs
    
    * prettier for migrate
    
    * can delete and write documents
    
    * can do transfer for tracks
    
    * little by little
    
    * better batch hopefully
    
    * upgrade lock

--- firebase-migration/src/migrate.mjs
+++ firebase-migration/src/migrate.mjs
@@ -1,94 +1,182 @@
-import { initializeApp } from "firebase/app";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
-import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import dataService from './nedb-interface.cjs';
+// https://firebase.google.com/docs/reference/admin
+// https://firebase.google.com/docs/reference/admin/node/firebase-admin.firestore
+// https://firebase.google.com/docs/reference/admin/node/firebase-admin.auth
+import { format } from "util";
+import { readFile } from "fs/promises";
+import admin from "firebase-admin";
+import { getApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+import dataService from "./nedb-interface.cjs";
 
-// TODO this should really be in typescript
-// TODO this should use the firebase-admin api
+const MAX_WRITES_PER_BATCH = 500; /** https://cloud.google.com/firestore/quotas#writes_and_transactions */
+const MIGRATE_USER = "sommernavi@gmail.com";
 
-const MIGRATE_USER = {
-  email: "sommernavi@gmail.com",
-  password: process.env.TRACKER_MIGRATION_PASSWORD,
-};
-
-let db;
-let auth;
-
-const initializeFirebase = async () => {
-  const firebaseConfig = {
-    apiKey: "AIzaSyASZh5cjJLkhXivX2DDzX69rhvj2mYBVCQ",
-    authDomain: "trackit-f1b79.firebaseapp.com",
-    projectId: "trackit-f1b79",
-    storageBucket: "trackit-f1b79.appspot.com",
-    messagingSenderId: "191629398412",
-    appId: "1:191629398412:web:b60e6f94b5c6e3d0f506a9",
-  };
-
-  const app = initializeApp(firebaseConfig);
-
-  db = getFirestore(app);
-  auth = getAuth(app);
-  await signInWithEmailAndPassword(
-    auth,
-    MIGRATE_USER.email,
-    MIGRATE_USER.password
-  );
-  return async () => {
-    await signOut(auth);
+class Batch {
+  constructor(db) {
+    this.__db = db;
+    this.__batches = [this.__db.batch()];
+    this.__counts = [0];
+    this.__index = 0;
   }
-};
-
-const loadTracksAndSteps = async () => {
-  const idMap = {};
-
-  const { _id: userId } = await dataService.users
-    .findOne({ email: MIGRATE_USER.email })
-    .execAsync();
-  idMap[userId] = auth.currentUser.uid;
-
-  const tracks = await dataService.tracks
-    .find({ userId })
-    .execAsync();
-  const steps = await dataService.steps
-    .find({ userId })
-    .execAsync();
-
-  const tracksRef = collection(db, "tracks");
-  const stepsRef = collection(db, "steps");
-
-  for (const track of tracks) {
-    track.userId = idMap[track.userId];
-    const oldId = track._id;
-    Reflect.deleteProperty(track, "_id");
-
-    let docRef = await addDoc(tracksRef, track);
-    idMap[oldId] = docRef.id;
-  }
-
-  for (const step of steps) {
-    step.userId = idMap[step.userId];
-    step.trackId = idMap[step.trackId];
-    // const oldId = step._id;
-    Reflect.deleteProperty(step, "_id");
-
-    if (step.trackId) {
-      await addDoc(stepsRef, step);
+  get batch() {
+    if (this.__counts[this.__index] >= MAX_WRITES_PER_BATCH) {
+      this.__batches.push(this.__db.batch());
+      this.__counts.push(0);
+      this.__index++;
     }
+    this.__counts[this.__index] += 1;
+    return this.__batches[this.__index];
+  }
+  create(...args) {
+    return this.batch.create(...args);
+  }
+  delete(...args) {
+    return this.batch.delete(...args);
+  }
+  async commit() {
+    return await Promise.all(this.__batches.map((batch) => batch.commit()));
+  }
+}
+
+const accountFilepath = new URL(
+  "../../temp/trackit-f1b79-firebase-adminsdk-5o81p-fff95050db.json",
+  import.meta.url
+);
+const readJsonFile = async (filepath) =>
+  JSON.parse(await readFile(filepath, "utf-8"));
+
+const chunk = (input, chunkSize) => {
+  const result = [];
+  for (let i = 0; i < input.length; i += chunkSize) {
+    result.push(input.slice(i, i + chunkSize));
+  }
+  return result;
+};
+
+const deleteCollection = async (db, colRef) => {
+  console.log("deleting collection '%s'", colRef.id);
+  const snapshot = await colRef.get();
+
+  const batch = new Batch(db);
+  for (const doc of snapshot.docs) {
+    batch.delete(doc.ref);
+  }
+  await batch.commit();
+  console.log("deleted %i records", snapshot.docs.length);
+};
+
+const createUserIdMap = async (app) => {
+  const auth = getAuth(app);
+  const userIdMap = {};
+
+  const oldUsers = await dataService.users.find({}).execAsync();
+  const { users: newUsers } = await auth.listUsers();
+  for (const oldUser of oldUsers) {
+    const newUser = newUsers.find((newUser) => newUser.email === oldUser.email);
+    userIdMap[oldUser._id] = {
+      oldUser,
+      ...(newUser && { uid: newUser.uid, newUser }),
+    };
+  }
+  return userIdMap;
+};
+
+const _mapIdField = (field, object, map) => {
+  if (!map[object[field]] || !map[object[field]].uid) {
+    throw new Error(
+      format(
+        "could not find matching uid for field %s id %s",
+        field,
+        object[field]
+      )
+    );
+  }
+  object[field] = map[object[field]].uid;
+};
+
+const transferTracks = async (app, userIdMap) => {
+  const db = getFirestore(app);
+  const trackIdMap = {};
+
+  const oldTracks = await dataService.tracks.find({}).execAsync();
+  const tracksColRef = db.collection("tracks");
+
+  const count = (await tracksColRef.count().get()).data().count;
+  if (count !== 0) {
+    await deleteCollection(db, tracksColRef);
   }
 
-}
+  const batch = new Batch(db);
+
+  for (const oldTrack of oldTracks) {
+    const newTrack = { ...oldTrack };
+    Reflect.deleteProperty(newTrack, "_id");
+    _mapIdField("userId", newTrack, userIdMap);
+
+    const newTrackRef = tracksColRef.doc();
+    batch.create(newTrackRef, newTrack);
+
+    trackIdMap[oldTrack._id] = { uid: newTrackRef.id, oldTrack, newTrack };
+  }
+  await batch.commit();
+  return trackIdMap;
+};
+
+const transferSteps = async (app, userIdMap, trackIdMap) => {
+  const db = getFirestore(app);
+  const stepIdMap = {};
+
+  const oldSteps = await dataService.steps.find({}).execAsync();
+  const stepsColRef = db.collection("steps");
+
+  const count = (await stepsColRef.count().get()).data().count;
+  if (count !== 0) {
+    await deleteCollection(db, stepsColRef);
+  }
+
+  const batch = new Batch(db);
+
+  for (const oldStep of oldSteps) {
+    if (!trackIdMap[oldStep.trackId]) {
+      console.log(
+        "skipping step %s with no associated track %s",
+        oldStep._id,
+        oldStep.trackId
+      );
+      continue;
+    }
+
+    const newStep = { ...oldStep };
+    Reflect.deleteProperty(newStep, "_id");
+    _mapIdField("userId", newStep, userIdMap);
+    _mapIdField("trackId", newStep, trackIdMap);
+
+    const newStepRef = stepsColRef.doc();
+    batch.create(newStepRef, newStep);
+
+    stepIdMap[oldStep._id] = { uid: newStepRef.id, oldStep, newStep };
+  }
+  await batch.commit();
+  return stepIdMap;
+};
 
 const main = async () => {
   console.log("initialize firebase");
-  const closeFirebase = await initializeFirebase();
-  let currentUser = auth.currentUser;
-  console.log(`logged in with ${currentUser.email}`);
+  const serviceAccount = await readJsonFile(accountFilepath);
 
-  console.log("loading tracks and steps");
-  await loadTracksAndSteps();
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+  const app = getApp();
 
-  await closeFirebase();
-  console.log("success");
+  console.log("creating user id map");
+  const userIdMap = await createUserIdMap(app);
+  console.log("transferring tracks");
+  const trackIdMap = await transferTracks(app, userIdMap);
+  console.log("transferred %i tracks", Object.keys(trackIdMap).length);
+  const stepIdMap = await transferSteps(app, userIdMap, trackIdMap);
+  console.log("transferred %i steps", Object.keys(stepIdMap).length);
 };
 
 main();
--- firebase-migration/src/nedb-interface.cjs
+++ firebase-migration/src/nedb-interface.cjs
@@ -1,10 +1,22 @@
 const pathlib = require("path");
 const DataStore = require("@seald-io/nedb");
 
-const DATA_DIR = pathlib.join(__dirname, "../../../backend/data");
+const DATA_DIR = pathlib.join(__dirname, "../../backend/data");
 
 module.exports = {
-  users: new DataStore({ filename: pathlib.join(DATA_DIR, "users.db"), autoload: true, timestampData: true }),
-  tracks: new DataStore({ filename: pathlib.join(DATA_DIR, "tracks.db"), autoload: true, timestampData: true }),
-  steps: new DataStore({ filename: pathlib.join(DATA_DIR, "steps.db"), autoload: true, timestampData: true }),
-}
+  users: new DataStore({
+    filename: pathlib.join(DATA_DIR, "users.db"),
+    autoload: true,
+    timestampData: true,
+  }),
+  tracks: new DataStore({
+    filename: pathlib.join(DATA_DIR, "tracks.db"),
+    autoload: true,
+    timestampData: true,
+  }),
+  steps: new DataStore({
+    filename: pathlib.join(DATA_DIR, "steps.db"),
+    autoload: true,
+    timestampData: true,
+  }),
+};
