922c01b3e8e35249fb8196cf1e0a59734aa0ba44
Richard Lindner <rlindner81@gmail.com>
2023-02-06 09:34:56 +0100

    minimal styling for byline

--- frontend/src/components/TrackList.vue
+++ frontend/src/components/TrackList.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { useUserStore } from "@/store/user";
 import { useStepStore } from "@/store/step";
+import { useCommonStore } from "@/store/common";
 
+const commonStore = useCommonStore();
 const userStore = useUserStore();
 const stepStore = useStepStore();
 </script>
@@ -9,7 +11,9 @@ const stepStore = useStepStore();
 <template>
   <div class="component track-list steps" v-if="stepStore.stepsDisplayRows && stepStore.stepsDisplayRows.length > 0">
     <div class="step" v-for="({ values, meta }, rowIndex) in stepStore.stepsDisplayRows" :key="rowIndex">
-      <div class="posted">{{ userStore.emailById(meta.postedBy) || meta.postedBy }} | {{ meta.postedAt }}</div>
+      <div class="posted" :class="{ other: meta.postedBy !== commonStore.userId }">
+        {{ userStore.emailById(meta.postedBy) || meta.postedBy }} | {{ meta.postedAt }}
+      </div>
       <div class="values">
         <div class="value" v-for="({ label, value }, valuesIndex) in values" :key="valuesIndex">
           <label>{{ label }}</label>
@@ -29,6 +33,11 @@ const stepStore = useStepStore();
     transition: all 0.15s ease-in-out;
 
     .posted {
+      padding: 0.25rem 0.5rem;
+      font-size: smaller;
+    }
+    .other {
+      text-align: end;
     }
 
     .values {
@@ -44,7 +53,7 @@ const stepStore = useStepStore();
     }
 
     label {
-      font-size: 0.87rem;
+      font-size: smaller;
       margin-bottom: 0.25rem;
     }
 
