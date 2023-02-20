9562913be93d477131df513b95d8c49477bdf967
Richard Lindner <rlindner81@gmail.com>
2023-02-20 11:21:58 +0100

    get ready for commit 3

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -2,21 +2,27 @@
 // TODO: second password field and minimal input validation
 import { ref } from "vue";
 import { register } from "@/firebase/auth";
-import LoadingButton from "@/components/LoadingButton.vue";
 
 let email = ref<string | null>(null);
 let password = ref<string | null>(null);
+let loading = ref(false);
+
+const doRegister = () => {
+  loading.value = true;
+  register({ email: email.value, password: password.value });
+  loading.value = false;
+};
 </script>
 
 <template>
-  <v-form ref="form" @submit.prevent="register">
+  <v-form ref="form" @submit.prevent="doRegister">
     <v-text-field label="E-mail" prepend-icon="mdi-account" v-model="email" required></v-text-field>
     <!--                :error-messages="errorMessages.email"-->
 
     <v-text-field label="Password" prepend-icon="mdi-lock" type="password" v-model="password" required></v-text-field>
 
     <div class="text-center">
-      <v-btn class="large text rounded" type="submit">Sign Up</v-btn>
+      <v-btn :loading="loading" class="large text rounded" type="submit">Sign Up</v-btn>
     </div>
     <div class="d-flex justify-start">
       <router-link :to="{ name: 'Login' }">Login</router-link>
