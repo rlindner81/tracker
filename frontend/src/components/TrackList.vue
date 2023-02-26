6dba945a84607abd1107f28dbff1767dbbcd7655
Richard Lindner <rlindner81@gmail.com>
2023-02-26 12:10:41 +0100

    pass over tracklist

--- frontend/src/components/TrackList.vue
+++ frontend/src/components/TrackList.vue
@@ -1,57 +1,30 @@
 <script setup lang="ts">
-import { useUserStore } from "@/store/user";
 import { useStepStore } from "@/store/step";
-import { useCommonStore } from "@/store/common";
+import { useTrackStore } from "@/store/track";
 import { computed } from "vue";
-// import { observeAuthChanges } from "@/firebase/auth";
-// import { he } from "vuetify/locale";
 
-const commonStore = useCommonStore();
-const userStore = useUserStore();
+const trackStore = useTrackStore();
 const stepStore = useStepStore();
 
 const headers = computed(() => {
-  let headers: any = [];
-  if (stepStore.stepsDisplayRows.length) {
-    stepStore.stepsDisplayRows[0].values.forEach((column) => {
-      const header = {
-        title: column.label,
-        align: "start",
-        sortable: true,
-        key: column.label,
-      };
-      headers.push(header);
-    });
-    headers.push({
-      title: "Tracked By",
-      align: "start",
-      sortable: true,
-      key: "postedBy",
-    });
-    headers.push({
-      title: "Tracked On",
-      align: "start",
-      sortable: true,
-      key: "postedAt",
-    });
-  }
-  return headers;
+  const trackFields = trackStore.current?.fields;
+  if (!trackFields) return [];
+  const metaFields = [
+    { name: "Tracked By", key: "postedBy" },
+    { name: "Tracked On", key: "postedAt" },
+  ];
+  return trackFields.concat(metaFields).map((field) => ({
+    title: field.name,
+    key: field.key,
+    align: "start",
+    sortable: true,
+  }));
 });
 
 const rows = computed(() => {
-  let rows: any = [];
-  if (stepStore.stepsDisplayRows.length) {
-    for (const { values, meta } of stepStore.stepsDisplayRows) {
-      const row = values.reduce((result, column) => {
-        result[column.label] = column.value;
-        return result;
-      }, {});
-      row["postedBy"] = userStore.emailById(meta.postedBy);
-      row["postedAt"] = meta.postedAt;
-      rows.push(row);
-    }
-  }
-  return rows;
+  return !stepStore.stepsDisplayRows.length
+    ? []
+    : stepStore.stepsDisplayRows.map(({ values, meta }) => ({ ...values, ...meta }));
 });
 </script>
 
@@ -65,20 +38,6 @@ const rows = computed(() => {
     class="elevation-1"
   />
   <v-btn class="mb-5 mr-5" position="fixed" location="bottom right" icon="mdi-plus" color="primary" />
-
-  <!--  <div class="component track-list steps" v-if="stepStore.stepsDisplayRows && stepStore.stepsDisplayRows.length > 0">-->
-  <!--    <div class="step" v-for="({ values, meta }, rowIndex) in stepStore.stepsDisplayRows" :key="rowIndex">-->
-  <!--      <div class="posted" :class="{ other: meta.postedBy !== commonStore.userId }">-->
-  <!--        {{ userStore.emailById(meta.postedBy) || meta.postedBy }} | {{ meta.postedAt }}-->
-  <!--      </div>-->
-  <!--      <div class="values">-->
-  <!--        <div class="value" v-for="({ label, value }, valuesIndex) in values" :key="valuesIndex">-->
-  <!--          <label>{{ label }}</label>-->
-  <!--          <span>{{ value }}</span>-->
-  <!--        </div>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </div>-->
 </template>
 
 <style></style>
