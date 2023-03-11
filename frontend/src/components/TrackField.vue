e0566e06485c0947c3fd55bad313b9f65f36d008
Jana Kasselmann <kasselmann.jana@gmail.com>
2023-03-11 15:09:47 +0100

    WIP track title & field component

---
+++ frontend/src/components/TrackField.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useTrackStore } from "@/store/track";
+import { useStepStore } from "@/store/step";
+import { TRACK_FIELD_INPUT, TRACK_FIELD_TYPE } from "@/constants";
+import { ref } from "vue";
+
+const trackStore = useTrackStore();
+</script>
+
+<template>
+    {{ 'This is my component.' }}
+</template>
+
+<style></style>
--- frontend/src/views/CreateTrack.vue
+++ frontend/src/views/CreateTrack.vue
@@ -1,18 +1,49 @@
 <script setup lang="ts">
+import { onBeforeMount, computed } from "vue";
 import { useTrackStore } from "@/store/track";
-import { useStepStore } from "@/store/step";
-
+import { TRACK_FIELD_TYPE, TRACK_FIELD_INPUT, TRACK_INPUT_TYPE } from "@/constants";
 import { useRoute } from "vue-router";
+import TrackField from "@/components/TrackField.vue";
+
+const trackStore = useTrackStore();
+const props = defineProps({
+  edit: {
+    default: false,
+  },
+});
+
+const relevant = computed(() => (props.edit ? trackStore.newUpdateTrack : trackStore.newCreateTrack));
+
+onBeforeMount(() => {
+  if (props.edit) {
+    trackStore.prepareNewUpdateTrack();
+  } else {
+    trackStore.prepareNewCreateTrack();
+  }
+});
 </script>
 
 <template>
-  <v-container>
-    <div>
-      <h1>{{ "This is my page." }}</h1>
-      <TrackList></TrackList>
-      <TrackSettings></TrackSettings>
-    </div>
+  <v-container v-if="relevant">
+    <v-card>
+      <v-card-text>
+        <v-text-field
+          v-model="relevant.name"
+          :label="$t('entity.track.name')"
+          :placeholder="$t('entity.track.namePlaceholder')"
+          density="compact"
+        ></v-text-field>
+        <v-text-field label="Track Description" placeholder="Enter a description ..." density="compact"></v-text-field>
+      </v-card-text>
+    </v-card>
+    <TrackField></TrackField>
   </v-container>
+  <v-card flat location="bottom" position="fixed" color="primary" :rounded="0" min-width="100%">
+    <v-card-actions>
+        <v-btn>Cancel</v-btn>
+        <v-btn color="success" variant="elevated">Save</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <style></style>
