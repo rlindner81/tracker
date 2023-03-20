a50ebfe4a1828a8845f91bf1f43b1054062ef48d
Richard Lindner <rlindner81@gmail.com>
2023-03-20 14:16:00 +0100

    more polish stuff 3

--- frontend/src/views/Tracker.vue
+++ frontend/src/views/Tracker.vue
@@ -27,19 +27,17 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div>
-    <v-card>
-      <v-card-title class="text-h5">{{ trackStore.titleById($route.params.track) }}</v-card-title>
-      <v-container>
-        <TrackList></TrackList>
-      </v-container>
-    </v-card>
+  <v-card class="py-4 fill-height">
+    <v-card-title class="text-h4">{{ trackStore.titleById($route.params.track) }}</v-card-title>
+    <v-container fluid>
+      <TrackList></TrackList>
+    </v-container>
 
     <v-btn @click="onAddStepClicked" class="mb-5 mr-5" position="fixed" location="bottom right" icon color="secondary">
       <v-icon>mdi-plus</v-icon>
       <AddStep></AddStep>
     </v-btn>
-  </div>
+  </v-card>
 </template>
 
 <style></style>
