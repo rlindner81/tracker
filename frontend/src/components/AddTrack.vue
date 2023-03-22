256c3024751490d1b0f813d394c36e5b37c13801
Richard Lindner <rlindner81@gmail.com>
2023-03-22 20:30:21 +0100

    allow changing select choice.value again

--- frontend/src/components/AddTrack.vue
+++ frontend/src/components/AddTrack.vue
@@ -245,7 +245,6 @@ const onFieldNameChange = (event, field) => {
                     density="compact"
                     hide-details="auto"
                     required
-                    disabled
                   ></v-text-field>
                 </v-col>
                 <v-col class="flex-grow-0 flex-shrink-1">
