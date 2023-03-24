31e8ae200b9ec0e2c1c52841db9592d460175a4a
Richard Lindner <rlindner81@gmail.com>
2023-03-24 18:42:33 +0100

    make build dynamically switch public directory, firebase config, and name

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="vite/client" />
-VITE_DEFAULT_LOCALE = en;
-VITE_FALLBACK_LOCALE = en;
+
+interface ImportMetaEnv {
+  readonly VITE_PUBLIC_DIR: string;
+  readonly VITE_DEFAULT_LOCALE: string;
+  readonly VITE_FALLBACK_LOCALE: string;
+  readonly VITE_APP_NAME: string;
+  readonly VITE_FIREBASE_CONFIG: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,65 +1,69 @@
 import { fileURLToPath, URL } from "node:url";
 import { resolve, dirname } from "node:path";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import vuetify from "vite-plugin-vuetify";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueI18nPlugin({
-      include: resolve(dirname(fileURLToPath(import.meta.url)), "./src/i18n/locales/**"),
-    }),
-    vuetify(),
-    VitePWA({
-      registerType: "autoUpdate",
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true,
-      },
-      manifest: {
-        name: "TrackIt",
-        short_name: "TrackIt",
-        description: "TrackIt allows you to track interesting or curious events in your life in a structured way.",
-        icons: [
-          {
-            src: "./img/icons/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "./img/icons/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-        start_url: "./",
-        display: "standalone",
-        background_color: "#f8f9fa",
-        theme_color: "#1b4965",
-      },
-    }),
-  ],
-  // https://github.com/vitejs/vite/issues/2204
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://0.0.0.0:8000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      vue(),
+      VueI18nPlugin({
+        include: resolve(dirname(fileURLToPath(import.meta.url)), "./src/i18n/locales/**"),
+      }),
+      vuetify(),
+      VitePWA({
+        registerType: "autoUpdate",
+        workbox: {
+          clientsClaim: true,
+          skipWaiting: true,
+        },
+        manifest: {
+          name: env.VITE_APP_NAME,
+          short_name: env.VITE_APP_NAME,
+          description: "TrackIt allows you to track interesting or curious events in your life in a structured way.",
+          icons: [
+            {
+              src: "./img/icons/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "./img/icons/android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+          start_url: "./",
+          display: "standalone",
+          background_color: "#f8f9fa",
+          theme_color: "#1b4965",
+        },
+      }),
+    ],
+    publicDir: env.VITE_PUBLIC_DIR,
+    // https://github.com/vitejs/vite/issues/2204
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
-  // see https://github.com/firebase/firebase-js-sdk/issues/6926
-  optimizeDeps: {
-    exclude: ["firebase/app", "firebase/auth", "firebase/firestore"],
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://0.0.0.0:8000",
+          changeOrigin: true,
+        },
+      },
+    },
+    // see https://github.com/firebase/firebase-js-sdk/issues/6926
+    optimizeDeps: {
+      exclude: ["firebase/app", "firebase/auth", "firebase/firestore"],
+    },
+  };
 });
