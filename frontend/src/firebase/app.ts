d52a03e60f9790b522367e010892dc0f29cce2b5
Richard Lindner <rlindner81@gmail.com>
2023-03-24 07:38:40 +0100

    dynamic switching of backend between dev and prod db for frontend

--- frontend/src/firebase/app.ts
+++ frontend/src/firebase/app.ts
@@ -1,12 +1,5 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBnndX-VxyIcXNEUD4njlhKAHEbYEadx7c",
-  authDomain: "trackit-dev-fde62.firebaseapp.com",
-  projectId: "trackit-dev-fde62",
-  storageBucket: "trackit-dev-fde62.appspot.com",
-  messagingSenderId: "587772691249",
-  appId: "1:587772691249:web:0b43c740d50202d3db2610",
-};
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 
 export const app = initializeApp(firebaseConfig);
